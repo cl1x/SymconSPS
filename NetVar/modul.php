d384ec5cddef7619296c92cebe94ec8e10815f0b
cl1x <fello94@web.de>
2015-11-18 23:12:45 +0100

    Update modul.php

--- NetVar/modul.php
+++ NetVar/modul.php
@@ -1,1 +1,18 @@
+<?
+	class NetVar extends IPSModule
+	{
 
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+
+			//Connect to available splitter or create a new one
+			$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+
+		}
+
+		
+
+	}
+?>
