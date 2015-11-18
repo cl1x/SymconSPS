4107d7453921f40fed5ab906a60fe8339f427213
cl1x <fello94@web.de>
2015-11-18 23:15:58 +0100

    Update modul.php

--- NetVar/modul.php
+++ NetVar/modul.php
@@ -7,7 +7,7 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 
-			//Connect to available splitter or create a new one
+			//Connect to available UDPSocket or create a new one
 			$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
 
 		}
