5e84242099e4c2c5f9ea0cb75d24ae5237de1a52
cl1x <fello94@web.de>
2015-11-19 13:43:38 +0100

    Update module.php

--- NetVar/module.php
+++ NetVar/module.php
@@ -10,6 +10,7 @@
 		 	$this->RegisterPropertyInteger("COBID", 0);
 		 	$this->RegisterPropertyInteger("VarNR", 0);
 		 	$this->RegisterPropertyInteger("VarType", 0);
+		 	$this->RegisterPropertyString("VarName","Variable");
 		 	
 		 }
 
@@ -21,6 +22,20 @@
 
 			//Connect to available UDPSocket or create a new one
 			$this->ConnectParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+			
+			
+			switch ($this->ReadPropertyInteger("VarType")) {
+				
+				case 0:
+       					break;
+				case 1:
+       					$variablenID = $this->RegisterVariableBoolean($this->ReadPropertyString("VarName"), "NetVar");
+					break;
+				case 2:
+					echo "i ist gleich 2";
+					break;
+				
+			}
 			
 		
 		}
